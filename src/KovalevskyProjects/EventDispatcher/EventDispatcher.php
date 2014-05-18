f1e634e7fac0e07885ddb6a01f713d36cf0737e3
Artur Kovalevsky <kovalevskyproj@gmail.com>
2014-05-18 12:10:54 +0300

    Added and implemented off, bind, unbind and has methods.

--- src/KovalevskyProjects/EventDispatcher/EventDispatcher.php
+++ src/KovalevskyProjects/EventDispatcher/EventDispatcher.php
@@ -58,4 +58,110 @@ class EventDispatcher implements EventDispatcherInterface
 
         return $this;
     }
+
+    /**
+     * Removes the action.
+     *
+     * @param string   $event  Event's name.
+     * @param callable $action The function that will be removed.
+     *
+     * @api
+     */
+    public function off($event, Callable $action)
+    {
+        if (!$this->has($event, $action)) {
+            return;
+        }
+
+        if (false === $key = array_search($action, $this->events[$event])) {
+            return;
+        }
+
+        unset ($this->events[$event][$key]);
+    }
+
+    /**
+     * Attaches the action for the more than one event.
+     *
+     * @param string|array $events The string with the names of the events,
+     *                             separated by the "space" or an array.
+     * @param callable     $action The action that will be called on the one
+     *                             of this events.
+     *
+     * @throws \InvalidArgumentException
+     * @api
+     */
+    public function bind($events, Callable $action = null)
+    {
+        if (is_string($events)) {
+            $events = $this->toArray($events);
+        }
+
+        if (null === $action) {
+            foreach ($events as $event => $action) {
+                if (!is_callable($action)) {
+                    throw new \InvalidArgumentException(
+                        'Action is must be callable'
+                    );
+                }
+
+                $this->on($event, $action);
+            }
+
+            return;
+        }
+
+        foreach ($events as $event) {
+            $this->on($event, $action);
+        }
+    }
+
+    /**
+     * Removes all previously attached actions.
+     *
+     * @param null|string|array $events The string with the names of the events,
+     *                                  separated with "space" or an array with
+     *                                  event names. NULL will remove all
+     *                                  attached actions
+     *
+     * @api
+     */
+    public function unbind($events = null)
+    {
+        if (null === $events) {
+            $this->events = array();
+            return;
+        }
+
+        if (is_string($events)) {
+            $events = $this->toArray($events);
+        }
+
+        foreach ($events as $event) {
+            unset ($this->events[$event]);
+        }
+    }
+
+    /**
+     * Checks whether the specified event has action.
+     *
+     * @param string   $event  Event's name
+     * @param callable $action Optional action.
+     *
+     * @return bool
+     * @api
+     */
+    public function has($event, Callable $action = null)
+    {
+        if ($action === null) {
+            return (isset($this->events[$event]));
+        }
+
+        return (false !== array_search($action, $this->events[$event]));
+    }
+
+    protected function toArray($events)
+    {
+        return preg_split('/[\s]+/', $events);
+    }
 }
--- src/KovalevskyProjects/EventDispatcher/EventDispatcherInterface.php
+++ src/KovalevskyProjects/EventDispatcher/EventDispatcherInterface.php
@@ -30,4 +30,49 @@ interface EventDispatcherInterface
      * @api
      */
     public function on($event, Callable $action);
+
+    /**
+     * Removes the action.
+     *
+     * @param string   $event  Event's name.
+     * @param callable $action The function that will be removed.
+     *
+     * @api
+     */
+    public function off($event, Callable $action);
+
+    /**
+     * Attaches the action for the more than one event.
+     *
+     * @param string|array $events The string with the names of the events,
+     *                             separated by the "space" or an array.
+     * @param callable     $action The action that will be called on the one
+     *                             of this events.
+     *
+     * @api
+     */
+    public function bind($events, Callable $action = null);
+
+    /**
+     * Removes all previously attached actions.
+     *
+     * @param null|string|array $events The string with the names of the events,
+     *                                  separated with "space" or an array with
+     *                                  event names. NULL will remove all
+     *                                  attached actions
+     *
+     * @api
+     */
+    public function unbind($events = null);
+
+    /**
+     * Checks whether the specified event has action.
+     *
+     * @param string   $event  Event's name
+     * @param callable $action Optional action.
+     *
+     * @return bool
+     * @api
+     */
+    public function has($event, Callable $action = null);
 }
